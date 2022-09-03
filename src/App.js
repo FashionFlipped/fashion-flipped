e02131f4e0ad8b75d1aac8fc5a386e6aadfeb047
Maxwell Stone <38924020+MadeWithStone@users.noreply.github.com>
2022-09-03 00:34:34 -0400

    add logout

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Auth from "./pages/Auth";
 import { useMutation, useQuery } from "./convex/_generated/react.ts";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const [userId, setUserId] = useState(null);
   const storeUser = useMutation("storeUser");
   const addChannel = useMutation("addChannel");
+  const { logout, user } = useAuth0();
   useEffect(() => {
     // Store the user in the database.
     // Recall that `storeUser` gets the user information via the `auth`
@@ -22,6 +24,9 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to React Router!</h1>
+      <button onClick={() => logout({ returnTo: window.location.origin })}>
+        Logout
+      </button>
       <Routes></Routes>
     </div>
   );
