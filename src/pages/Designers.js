9c969eaa4c9a714f9ce510ceef69cec5739acc74
Umang <jumang.jain@gmail.com>
2022-09-04 01:26:46 -0400

    designers

--- src/pages/Designers.js
+++ src/pages/Designers.js
@@ -1,1 +1,16 @@
 import React, { Component } from 'react'
+
+const About = () => {
+    return (
+      <><div>
+            <h1>
+                Are you a budding designer?
+            </h1>
+        </div><div>
+                <h3> We need you!</h3>
+                <p> At FashionFlipped we will pay you to do what you love - redesign clothe into new trendy fashion! All while you build your portfolio and transition the world to sustainable fashion :)</p>
+            </div></>
+    );
+  };
+    
+  export default About;
